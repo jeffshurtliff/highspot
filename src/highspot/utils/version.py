a60215315c239f1fa5879c58951f93a301e8e33d
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2022-10-10 15:54:12 -0600

    Added log_utils.py and version.py

---
+++ src/highspot/utils/version.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            highspot.utils.version
+:Synopsis:          This simple script contains the package version
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     10 Oct 2022
+"""
+
+from . import log_utils
+
+# Initialize logging
+logger = log_utils.initialize_logging(__name__)
+
+# Define special and global variables
+__version__ = "1.0.0dev1"
+
+
+def get_full_version():
+    """This function returns the current full version of the ``highspot`` package.
+
+    .. versionadded:: 1.0.0
+    """
+    return __version__
+
+
+def get_major_minor_version():
+    """This function returns the current major.minor (i.e. X.Y) version of the ``highspot`` package."""
+    return ".".join(__version__.split(".")[:2])
