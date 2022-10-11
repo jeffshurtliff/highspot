50093a437843760cc5d4a8597de974e59a821704
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2022-10-11 14:19:45 -0600

    Added several new methods and functions

--- src/highspot/core.py
+++ src/highspot/core.py
@@ -8,6 +8,7 @@
 """
 
 from . import api
+from . import users as users_module
 from .errors import exceptions
 from .utils import log_utils, version
 
@@ -38,6 +39,16 @@ class Highspot(object):
             raise exceptions.MissingAuthDataError('password')
         self.auth = (username, password)
 
+        # Import inner object classes so their methods can be called from the primary object
+        self.users = self._import_users_class()
+
+    def _import_users_class(self):
+        """This method allows the :py:class:`highspot.core.Highspot.Users` class to be utilized in the core object.
+
+        .. versionadded:: 1.0.0
+        """
+        return Highspot.User(self)
+
     # Define the basic GET request method
     def get(self, endpoint, return_json=True, verify_ssl=True):
         """This function performs a GET request and will retry several times if a failure occurs.
@@ -54,3 +65,50 @@ class Highspot(object):
         :raises: :py:exc:`highspot.errors.exceptions.APIConnectionError`
         """
         return api.get_request_with_retries(self, endpoint, return_json, verify_ssl)
+
+    class User(object):
+        """This class includes methods associated with Freshservice tickets."""
+        def __init__(self, hs_object):
+            """This method initializes the :py:class:`highspot.core.Highspot.Tickets` inner class object.
+
+            .. versionadded:: 1.0.0
+
+            :param hs_object: The core :py:class:`highspot.Highspot` object
+            :type hs_object: class[highspot.Highspot]
+            """
+            self.hs_object = hs_object
+
+        def me(self):
+            """This function returns the information about the user making the API call.
+
+            .. versionadded:: 1.0.0
+
+            :returns: A dictionary with the user data
+            :raises: :py:exc:`highspot.errors.exceptions.APIConnectionError`
+            """
+            return users_module.me(self.hs_object)
+
+        def get_users(self, email=None, list_type=None, with_fields=None, exclude_fields=None, start=0, limit=100):
+            """This function retrieves a list of users.
+
+            .. versionadded:: 1.0.0
+
+            :param email: An email address by which to filter the users
+            :type email: str, None
+            :param list_type: Allows filtering by ``all`` or ``unverified`` users (filters by ``verified`` users by default)
+            :type list_type: str, None
+            :param with_fields: Additional field(s) to include in the response
+            :type with_fields: str, tuple, list, set, None
+            :param exclude_fields: Additional field(s) to exclude in the response
+            :type exclude_fields: str, tuple, list, set, None
+            :param start: The start position of a paged request (``0`` by default)
+            :type start: int, str
+            :param limit: Maximum number of users returned (``100`` by default)
+            :type limit: int, str
+            :returns: A list of dictionaries containing the user data
+            :raises: :py:exc:`highspot.errors.exceptions.InvalidFieldError`,
+                     :py:exc:`highspot.errors.exceptions.APIConnectionError`
+            """
+            return users_module.get_users(self.hs_object, email=email, list_type=list_type, with_fields=with_fields,
+                                          exclude_fields=exclude_fields, start=start, limit=limit)
+
---
+++ src/highspot/users.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            highspot.users
+:Synopsis:          Defines the users-related functions associated with the Highspot API
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     11 Oct 2022
+"""
+
+from . import api
+from .errors import exceptions
+
+
+def me(hs_object):
+    """This function returns the information about the user making the API call.
+
+    .. versionadded:: 1.0.0
+
+    :param hs_object: The core :py:class:`highspot.Highspot` object
+    :type hs_object: class[highspot.Highspot]
+    :returns: A dictionary with the user data
+    :raises: :py:exc:`highspot.errors.exceptions.APIConnectionError`
+    """
+    return api.get_request_with_retries(hs_object, '/me')
+
+
+def get_users(hs_object, email=None, list_type=None, with_fields=None, exclude_fields=None, start=0, limit=100):
+    """This function retrieves a list of users.
+
+    .. versionadded:: 1.0.0
+
+    :param hs_object: The core :py:class:`highspot.Highspot` object
+    :type hs_object: class[highspot.Highspot]
+    :param email: An email address by which to filter the users
+    :type email: str, None
+    :param list_type: Allows filtering by ``all`` or ``unverified`` users (filters by ``verified`` users by default)
+    :type list_type: str, None
+    :param with_fields: Additional field(s) to include in the response
+    :type with_fields: str, tuple, list, set, None
+    :param exclude_fields: Additional field(s) to exclude in the response
+    :type exclude_fields: str, tuple, list, set, None
+    :param start: The start position of a paged request (``0`` by default)
+    :type start: int, str
+    :param limit: Maximum number of users returned (``100`` by default)
+    :type limit: int, str
+    :returns: A list of dictionaries containing the user data
+    :raises: :py:exc:`highspot.errors.exceptions.InvalidFieldError`,
+             :py:exc:`highspot.errors.exceptions.APIConnectionError`
+    """
+    endpoint = '/users?'
+    if email and isinstance(email, str):
+        endpoint += f'email={email}'
+    if list_type:
+        if list_type == 'all' or list_type == 'unverified' or list_type == 'verified':
+            endpoint += f'list={list_type}' if endpoint.endswith('?') else f'&list={list_type}'
+        else:
+            raise exceptions.InvalidFieldError(val='list_type')
+    if with_fields:
+        # TODO: Raise an exception if with_fields is not a str, tuple, list, or set
+        if not isinstance(with_fields, str):
+            with_fields = ",".join(with_fields)
+        segment = f'with-fields={with_fields}'
+        endpoint += segment if endpoint.endswith('?') else f'&{segment}'
+    if exclude_fields:
+        # TODO: Raise an exception if exclude_fields is not a str, tuple, list, or set
+        if not isinstance(exclude_fields, str):
+            exclude_fields = ",".join(exclude_fields)
+        segment = f'exclude-fields={exclude_fields}'
+        endpoint += segment if endpoint.endswith('?') else f'&{segment}'
+    start_limit_segment = f'start={start}&limit={limit}'
+    endpoint += start_limit_segment if endpoint.endswith('?') else f'&{start_limit_segment}'
+    return api.get_request_with_retries(hs_object, endpoint)
